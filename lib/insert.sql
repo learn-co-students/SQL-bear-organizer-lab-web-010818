ceda8dbbf19279cf2e6a86d1ea70a1274549d134
GuttermanA <alexanderf.gutterman@gmail.com>
2018-01-14 13:36:00 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Mr. Chocolate', 'M', 'BROWN','FORCEFUL',1);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Rowdy','F','PURPLE','ROWDY',1);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Tabitha','F','BLACK','DOPY',0);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Sergeant Brown','M','HOT PINK','MURDEROUS',1);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Melissa','M','SKY BLUE','SAD',1);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Grinch','M','GREEN','GRUMPY',0);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES('Wendy','F','RED','SILLY',1);
+
+INSERT INTO bears(name,gender,color,temperament,alive)
+VALUES(NULL,'M','ORANGE','DUMB',0);
