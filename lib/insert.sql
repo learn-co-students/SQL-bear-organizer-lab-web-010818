815a16ebd5a601941ea0243dab70b538f9a7107b
MinesJA <minesja@gmail.com>
2018-01-15 18:52:20 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+ (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Docile', 'Y'),
+ (2, 'Rowdy', 7, 'M', 'Black', 'Aggressive', 'Y'),
+ (3, 'Tabitha', 2, 'F', 'Brown', 'Friendly', 'Y'),
+ (4, 'Sergeant Brown', 3, 'M', 'Gray', 'goofy', 'N'),
+ (5, 'Melissa', 1, 'F', 'Black', 'Aggressive', 'Y'),
+ (6, 'Grinch', 9, 'M', 'Black', 'Anti-Social', 'N'),
+ (7, 'Wendy', 10, 'F', 'White', 'Friendly', 'Y'),
+ (8, NULL, 15, 'F', 'Brown', 'Aggressive', 'N');
+
+ --
+ -- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+ -- (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+ -- (2,"Rowdy", 10, "M", "black", "intense", 1),
+ -- (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+ -- (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+ -- (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+ -- (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+ -- (7,"Wendy", 6, "F", "Blue", "naive", 1),
+ -- (8,null, 20, "M", "black", "aggressive", 0);
