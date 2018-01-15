858867ab288eda5a0e0b0fa75049475378743f6f
Fei <fei.hafferkamp@gmail.com>
2018-01-14 23:44:48 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,128 @@
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Mr. Chocolate",
+	34,
+	"M",
+	"Brown",
+	"Sweet",
+	1
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Rowdy",
+	5,
+	"M",
+	"Rainbow",
+	"Curious",
+	1
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Tabitha",
+	28,
+	"F",
+	"Black",
+	"Fierce",
+	0
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Sergeant Brown",
+	14,
+	"M",
+	"Brown",
+	"Serious",
+	0
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Melissa",
+	64,
+	"F",
+	"White",
+	"Flirty",
+	1
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Grinch",
+	16,
+	"M",
+	"Green",
+	"Mean",
+	1
+	);
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	"Wendy",
+	37,
+	"F",
+	"Red",
+	"Fun",
+	1
+	);
+
+
+INSERT INTO bears (
+	name, 
+	age, 
+	gender, 
+	color, 
+	temperament, 
+	alive) 
+VALUES(
+	NULL,
+	55,
+	"F",
+	"Brown",
+	"Murderous",
+	1
+	);
