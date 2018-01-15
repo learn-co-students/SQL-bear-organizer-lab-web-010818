b953a6a88a29625561da2f81a1b5ac11ad47ade1
Raskovan <alexey.katalkin@gmail.com>
2018-01-14 21:57:22 -0500

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 2, 'M', 'brown', 'wild', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'black', 'gentle', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'white', 'noisy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 12, 'M', 'red', 'calm', 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 1, 'F', 'purple', 'loving', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 10, 'M', 'brown', 'wild', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 23, 'F', 'brown', 'sleepy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 42, 'F', 'yellow', 'loving', 1);
